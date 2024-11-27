8d9db29d11c01fca5f2f1b2be050529740c13d71
sohailshah20 <contact.sohailali@gmail.com>
2024-11-27 13:07:56 +0530

    minor fixes/added shift filter to orders

--- packages/scripts/dumpToDevTables.js
+++ packages/scripts/dumpToDevTables.js
@@ -1,4 +1,4 @@
-import { findAll, save } from "../functions/common/data.js";
+import { findAll, save, update } from "../functions/common/data.js";
 // const runsheets = await findAll("prod-promodeagro-admin-runsheetTable");
 
 // let k = 0;
@@ -8,17 +8,31 @@ import { findAll, save } from "../functions/common/data.js";
 // }
 
 // const products = await findAll("prod-promodeagro-admin-productsTable");
-const inventory = await findAll("prod-promodeagro-admin-inventoryTable");
+const orders = await findAll("dev-promodeagro-admin-OrdersTable");
 
 // let i = 0;
 // for (const product of products.items) {
 // 	console.log(`ADDING PRODUCTS ${i++}`);
 // 	await save("dev-promodeagro-admin-productsTable", product);
 // }
+let s = 0;
+const getShift = () => {
+	const shift = ["morning", "evening"];
+	s = s == 0 ? 1 : 0;
+	return shift[s];
+};
+
 let j = 0;
-for (const inven of inventory.items) {
+for (const order of orders.items) {
 	console.log(`ADDING INVENTORY ${j++}`);
-	await save("dev-promodeagro-admin-inventoryTable", inven);
+	order.deliverySlot.shift = getShift();
+	await update(
+		"dev-promodeagro-admin-OrdersTable",
+		{ id: order.id },
+		{
+			deliverySlot: order.deliverySlot,
+		}
+	);
 }
 
 // const runsheets = await findAll("prod-promodeagro-admin-OrdersTable");
